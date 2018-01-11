b9fe827d6fc5929eef496d87337ccd338b76cfdb
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-11 10:20:57 -0600

    file setup continues apace

---
+++ javascripts/areaAttractions.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Given user wants to see the attraction list of an area
+
+// When the user clicks on an area
+
+// Then all attraction names assigned to that area should be listed in the left 30% of the screen
+// And the attraction type should be in parenthesis next to the name
+// And the name should be a hyperlink
