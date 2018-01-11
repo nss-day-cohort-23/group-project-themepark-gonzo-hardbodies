9b607b9dcc5199c85988c918d51e3d052539792c
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-11 14:08:20 -0600

    user click link

--- javascripts/areaAttractions.js
+++ javascripts/areaAttractions.js
@@ -5,7 +5,7 @@ function outputToDom(attractionArray){
     let currentShow = document.getElementById("currentShows");
     currentShow.innerHTML="";
     for (let i = 0; i < attractionArray.length; i++){
-        currentShow.innerHTML+= `<p><a>${attractionArray[i]}</a></p>`;
+        currentShow.innerHTML+= `<p><a href="#">${attractionArray[i]}</a></p>`;
     }
 }
 
