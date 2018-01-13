9a0c81e678248e338582bb136733098c961f1e41
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-13 12:25:18 -0600

    maintain space in dom w/ css

--- dist/app.js
+++ dist/app.js
@@ -141,16 +141,16 @@ module.exports.outputToDom= (attractionArray) =>{
     for (let i = 0; i < attractionArray.length; i++) {
         if (attractionArray[i].hasOwnProperty('times')){
             console.log("help");
-            currentShow.innerHTML+=currentShow.innerHTML += `<div><h4><a href="#">${attractionArray[i].name}</a><p style="display:none;" class="descrip">${attractionArray[i].description}${attractionArray[i].times}</p><h4></div>`;
+            currentShow.innerHTML += `<div><h4><a href="#">${attractionArray[i].name}</a><p style="display:none;" class="descrip">${attractionArray[i].description}${attractionArray[i].times}</p><h4></div>`;
         } else {
-            currentShow.innerHTML += `<div><h4><a href="#">${attractionArray[i].name}</a><p style="display:none;" class="descrip">${attractionArray[i].description}</p><h4></div>`;
+            currentShow.innerHTML += `<div><h4><a href="#">${attractionArray[i].name}</a><p style="visibility:hidden;" class="descrip">${attractionArray[i].description}</p><h4></div>`;
 
         }
     }
 
     $(document).on("click",event.target, function(){
         console.log("et", event.target);
-        $(event.target).next().first("p:hidden").show();
+        $(event.target).next().first("p:hidden").css('visibility','visible');
         console.log("first event.next", $(event.target).next());
         }
     )
