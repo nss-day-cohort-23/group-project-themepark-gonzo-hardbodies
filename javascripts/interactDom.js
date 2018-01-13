64fb9879328ce383ebfeeab66e515f2b404709a9
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-13 06:19:35 -0600

    hiding div but only want to hide p element

--- javascripts/interactDom.js
+++ javascripts/interactDom.js
@@ -5,17 +5,18 @@ module.exports.outputToDom= (attractionArray) =>{
     let currentShow = document.getElementById("currentShows");
     currentShow.innerHTML = "";
     for (let i = 0; i < attractionArray.length; i++) {
-        currentShow.innerHTML += `<div id=d${i}><h4 id=h${i}><a id=${i} href="#">${attractionArray[i].name}</a><p id=p${i} style="display:none;">${attractionArray[i].description}</p><h4></div>`;
+        currentShow.innerHTML += `<div id=d${i}><h4 id=h${i}><a id=${i} href="#">${attractionArray[i].name}</a><p id=p${i} style="display:none;" class="descrip">${attractionArray[i].description}</p><h4></div>`;
     }
 
     $(document).on("click",event.target, function(){
         console.log("et", event.target);
         $(event.target).next().first("p:hidden").show();
+        console.log("first event.next", $(event.target).next());
         }
-    );
-    // .on("click",event.target,function(){
-    //     if ($(event.target).id != );
-    // });
+    )
+    .on("click",event.target,function(){
+        $(event.target).parent().parent().prev().first("p:hidden").hide();
+    });
 
 };
 
