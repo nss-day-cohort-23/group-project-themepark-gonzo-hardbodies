81d97268aafcb8eb265a0c9bf31b17f0b5129e9a
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-12 19:02:03 -0600

    remove console log

--- javascripts/interactDom.js
+++ javascripts/interactDom.js
@@ -3,7 +3,6 @@ let $ = require("jquery");
 
 module.exports.outputToDom= (attractionArray) =>{
     let currentShow = document.getElementById("currentShows");
-    console.log(attractionArray, "attraction array what value do I need for descrip");
     currentShow.innerHTML = "";
     for (let i = 0; i < attractionArray.length; i++) {
         currentShow.innerHTML += `<div id=${i}><h4 id=${i}><a href="#">${attractionArray[i].name}<li style="display:none;">${attractionArray[i].description}</li></a><h4></div>`;
