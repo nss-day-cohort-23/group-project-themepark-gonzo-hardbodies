146df3b264dcfd7454183d1c422e4e9b04c47ec5
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-13 14:25:40 -0600

    only et descrip show

--- dist/app.js
+++ dist/app.js
@@ -141,22 +141,25 @@ module.exports.outputToDom= (attractionArray) =>{
     for (let i = 0; i < attractionArray.length; i++) {
         if (attractionArray[i].hasOwnProperty('times')){
             console.log("help");
-            currentShow.innerHTML += `<div><h4><a href="#">${attractionArray[i].name}</a><p style="display:none;" class="descrip">${attractionArray[i].description}${attractionArray[i].times}</p><h4></div>`;
+            currentShow.innerHTML += `<div><h4><a class="link" href="#">${attractionArray[i].name}</a><p class="descrip">${attractionArray[i].description}${attractionArray[i].times}</p><h4></div>`;
         } else {
-            currentShow.innerHTML += `<div><h4><a href="#">${attractionArray[i].name}</a><p style="visibility:hidden;" class="descrip">${attractionArray[i].description}</p><h4></div>`;
+            currentShow.innerHTML += `<div><h4><a href="#">${attractionArray[i].name}</a><p class="descrip">${attractionArray[i].description}</p><h4></div>`;
 
         }
     }
 
-    $(document).on("click",event.target, function(){
+    $(document).on("click",$(".link"), function(){
+        console.log("you clicked on link");
         console.log("et", event.target);
-        $(event.target).next().first("p:hidden").css('visibility','visible');
-        console.log("first event.next", $(event.target).next());
+        // $(event.target).next().first("p:hidden").css('visibility','visible');
+        // console.log("first event.next", $(event.target).next());
+        $(".descrip").hide();
+        $(event.target).siblings(".descrip").show();
         }
-    )
-    .on("click",event.target,function(){
-        $(event.target).parent().parent().prev().first("p:hidden").css('visibility','hidden');
-    });
+    );
+    // .on("click",event.target,function(){
+    //     $(event.target).parent().parent().prev().first("p:hidden").css('visibility','hidden');
+    // });
 
 };
 
