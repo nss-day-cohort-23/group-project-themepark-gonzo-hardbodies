d97e2c6ca0c52d781bbfa806b2dca84bfb218413
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-13 11:57:36 -0600

    times showing but the lag is very strong

--- javascripts/interactDom.js
+++ javascripts/interactDom.js
@@ -5,7 +5,13 @@ module.exports.outputToDom= (attractionArray) =>{
     let currentShow = document.getElementById("currentShows");
     currentShow.innerHTML = "";
     for (let i = 0; i < attractionArray.length; i++) {
-        currentShow.innerHTML += `<div id=d${i}><h4 id=h${i}><a id=${i} href="#">${attractionArray[i].name}</a><p id=p${i} style="display:none;" class="descrip">${attractionArray[i].description}${attractionArray[i].times}</p><h4></div>`;
+        if (attractionArray[i].hasOwnProperty('times')){
+            console.log("help");
+            currentShow.innerHTML+=currentShow.innerHTML += `<div id=d${i}><h4 id=h${i}><a id=${i} href="#">${attractionArray[i].name}</a><p id=p${i} style="display:none;" class="descrip">${attractionArray[i].description}${attractionArray[i].times}</p><h4></div>`;
+        } else {
+            currentShow.innerHTML += `<div id=d${i}><h4 id=h${i}><a id=${i} href="#">${attractionArray[i].name}</a><p id=p${i} style="display:none;" class="descrip">${attractionArray[i].description}</p><h4></div>`;
+
+        }
     }
 
     $(document).on("click",event.target, function(){
