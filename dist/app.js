477cee452b5a12d343f49c6f4b3934eb5cb32306
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-13 08:30:35 -0600

    display hours for attraction

--- dist/app.js
+++ dist/app.js
@@ -139,7 +139,7 @@ module.exports.outputToDom= (attractionArray) =>{
     let currentShow = document.getElementById("currentShows");
     currentShow.innerHTML = "";
     for (let i = 0; i < attractionArray.length; i++) {
-        currentShow.innerHTML += `<div id=d${i}><h4 id=h${i}><a id=${i} href="#">${attractionArray[i].name}</a><p id=p${i} style="display:none;" class="descrip">${attractionArray[i].description}</p><h4></div>`;
+        currentShow.innerHTML += `<div id=d${i}><h4 id=h${i}><a id=${i} href="#">${attractionArray[i].name}</a><p id=p${i} style="display:none;" class="descrip">${attractionArray[i].description}${attractionArray[i].times}</p><h4></div>`;
     }
 
     $(document).on("click",event.target, function(){
