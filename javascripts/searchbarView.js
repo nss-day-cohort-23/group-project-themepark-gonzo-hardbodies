917f7b7f083e1f0d65b0e56e82e873f28c8d2c3f
Slade Utley <slade.utley@gmail.com>
2018-01-15 09:52:03 -0600

    set up requirement intstructions in comments

--- javascripts/searchbarView.js
+++ javascripts/searchbarView.js
@@ -5,6 +5,8 @@ let searchbar = require('./searchbar');
 //want name to equal area id - USE REGULAR EXPRESSIONS - did it but not that way;
 //want bordered area to be unbordered when new attraction is searched;
 //if user puts in pirates, it shows all the attractions with pirates in it;
+//also would be nice if it showed suggestions as you type
+// Zanzibar Trading Co
 
 let mapSection;
 
@@ -14,8 +16,11 @@ let highlightAreaofSearchedAttractionAndOutputInfo = (attractions) => {
     let userText = document.getElementById("userInput");
     for (let i=0; i<attractionsAreaIdNameAndDescription.length; i++) {
         if (userText.value === attractionsAreaIdNameAndDescription[i]) {
+        // if (attractionsAreaIdNameAndDescription[i].includes(userText.value)) {
             let correspondingId = attractionsAreaIdNameAndDescription[i + 1];
             let mapSection = document.getElementById(`mapArea${correspondingId}`);
+            mapSection.style.border = "none";
+            console.log('map section', mapSection);
             mapSection.style.border = "2px solid black";
             let output = document.getElementById("currentShows");
             output.innerHTML = `
